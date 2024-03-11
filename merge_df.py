63cd15c19b06d3a068e0311af2017d00013c8414
fasthill <kangeun@naver.com>
2024-03-11 17:27:31 +0900

    insert missing date from 2022.12.27 to 2023.1.7

--- merge_df.py
+++ merge_df.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-def merge_df(df_old, df_add, subset_col):
+def merge_df(df_old, df_add, sel_col):
     df_merge = pd.concat([df_old, df_add], ignore_index=True)
-    df_merge.drop_duplicates(subset=[subset_col], keep='last', inplace=True)
+    df_merge.drop_duplicates(subset=[df_old.columns[sel_col]], keep='last', inplace=True)
     df_merge.sort_values(by=[df_merge.columns[0]], inplace=True)
     df_merge.index = np.arange(0, len(df_merge))  # 일련 번호 오름차순으로 재 설정
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     df_old = pd.read_csv('data/test/d_old.csv',  index_col=0)
     df_add = pd.read_csv('data/test/d_new.csv',  index_col=0)
 
-    df_merge = merge_df(df_old, df_add, 'date')
+    df_merge = merge_df(df_old, df_add, 0)
     df_merge.to_csv('data/test/d_merge.csv')
     df_merge.to_pickle('data/test/d_merge.pkl')
 
---
+++ update_historical_data.py
@@ -0,0 +1,67 @@
+from selenium import webdriver as wd
+from selenium.webdriver import ActionChains # scroll down 사용하기 위하여 선서
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from bs4 import BeautifulSoup as bs
+
+import datetime, time
+from datetime import date
+
+import pandas as pd
+import numpy as np
+import requests
+import time
+import os, sys, io
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from open_browser import open_browser
+from set_current_unit import set_current_unit
+from open_window_individual_stock_trend import open_window_individual_stock_trend
+from set_date_n_search import set_date_n_search
+from get_data_table import get_data_table
+from merge_df import merge_df
+
+from data.constant.constants import COMPANY_CODE
+code = COMPANY_CODE
+
+# open browser
+main_url = 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020103'
+# 개별종목 시세추이 data-menu-id: MDC0201020103
+driver = open_browser()
+driver.get(main_url)
+
+set_current_unit(driver)
+
+data_directory = 'data/trend_data/'
+total = len(code)
+
+for i, (key, val) in enumerate(code.items()):
+    com_name = "/".join([key, val[0]])
+    pkl_name = '{}_historical.pkl'.format(val[1])
+    try:
+        df_o = pd.read_pickle(data_directory + pkl_name)
+        start_date = df_o['date'].iloc[len(df_o) - 1]
+    except:
+        start_date = datetime.date(2022, 1, 1)  # 데이터 취득 시작 일자
+
+    open_window_individual_stock_trend(driver, com_name)
+
+    start_date = datetime.date(2022, 12, 27)
+    end_date = datetime.date(2023, 1, 7)
+    start_str = start_date.strftime('%Y-%m-%d')
+    end_str = end_date.strftime('%Y-%m-%d')
+    set_date_n_search(driver, start_str, end_str)
+
+    df_get = get_data_table(driver)
+
+    df_o = merge_df(df_o, df_get, 0) # 0번 칼럼 기준으로 정렬
+
+    df_o.to_pickle(data_directory + pkl_name)
+    df_o.to_csv(data_directory + pkl_name.replace('pkl', 'csv'))
+
+    print(com_name, f'{i + 1}/{total}', end=', ')  # 진행상황 확인용
