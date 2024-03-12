f4d3f1e25554469876e582a3b626c20dbc38514a
fasthill <kangeun@naver.com>
2024-03-12 16:40:50 +0900

    modify get investors data table

--- get_investors_data_table.py
+++ get_investors_data_table.py
@@ -14,11 +14,39 @@ from open_investors_window import open_investors_window
 from open_window_investors_data import open_window_investors_data
 from set_date_n_search import set_date_n_search
 
+def convert_date(s_date, e_date):
+    date_range_ts = pd.date_range(start=s_date, end=e_date)
+    date_range = []
+    for x in date_range_ts:
+        date_range.append(datetime.datetime.strftime(x, "%Y%m%d"))
+    return date_range
 def get_investors_data_table(driver, s_date, e_date):  # table data 취득 and return dataframe
     # 여기에 set data n search를 같이 집어 넣어서 table를 찾아야 함.
     # 매일 매일의 테이블을 append 시켜서 한개의 table로 만들어 반환해야함.
 
-    return df_get_c
+    base_data_directory = './data/base_data/stock_market_holydays/'
+    opening_days_kor = pd.read_pickle(base_data_directory+'opening_days_kor.pkl') # 한국 개장일 데이터
+
+    start_str = s_date.strftime('%Y-%m-%d')
+    end_str = e_date.strftime('%Y-%m-%d')
+
+    date_range = convert_date(start_date, end_date)
+
+    for datei in date_range:
+
+        date = datetime.datetime.strptime(datei, "%Y%m%d").date()
+        if date in list(opening_days_kor):
+            return True
+            print(date)
+        else:
+            return False
+
+        if not is_opening_day(datei):
+            continue
+    # for
+    # set_date_n_search(driver, start_str, end_str, 2)
+
+    return # df_get_c
 
 
 if __name__ == '__main__':
@@ -40,11 +68,11 @@ if __name__ == '__main__':
     # date형식 변환후 입력
     start_date = datetime.date(2021, 12, 27)
     end_date = datetime.date(2024, 3, 7)
-    start_str = start_date.strftime('%Y-%m-%d')
-    end_str = end_date.strftime('%Y-%m-%d')
-    set_date_n_search(driver, start_str, end_str, 2)
+    # start_str = start_date.strftime('%Y-%m-%d')
+    # end_str = end_date.strftime('%Y-%m-%d')
+    # set_date_n_search(driver, start_str, end_str, 2)
 
-    df = get_investors_data_table(driver)
+    df = get_investors_data_table(driver, start_date, end_date)
 
     df.to_csv('sec.csv')
 
