928c02021c1b70072352888f43a4ed3d7e6ba1d5
fasthill <kangeun@naver.com>
2024-03-19 14:15:14 +0900

    read pickle

---
+++ read_pickle_csv_data.py
@@ -0,0 +1,33 @@
+
+import datetime
+
+import pandas as pd
+
+from open_browser import open_browser
+from set_current_unit import set_current_unit
+from set_date_n_search_inv import set_date_n_search_inv
+from get_investors_data_table import get_investors_data_table
+from open_window_investors_data import open_window_investors_data
+from open_investors_window import open_investors_window
+from merge_df import merge_df
+
+from data.constant.constants import COMPANY_CODE
+code = COMPANY_CODE
+
+data_directory = 'data/investors_data/'
+total = len(code)
+
+for i, (key, val) in enumerate(code.items()):
+    com_name = "/".join([key, val[0]])
+    pkl_name = '{}_investors.pkl'.format(val[1])
+    df_o = pd.read_pickle(data_directory + pkl_name)
+
+    df_col = ['date', 'retail', 'foreigner', 'institution', 'financial', 'invtrust',
+              'pension', 'privequity', 'bank', 'insurance', 'financeetc',
+              'corporateetc', 'foreigneretc']
+    df_o = df_o[df_col]
+
+    df_o.to_pickle(data_directory + pkl_name)
+    df_o.to_csv(data_directory + pkl_name.replace('pkl', 'csv'))
+
+    print(com_name, f'{i + 1}/{total}', end=', ')  # 진행상황 확인용
